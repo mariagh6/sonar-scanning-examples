a32c4baea6e667595877d24020de2e3e35f61ea7
ELCANET\gomm <trinidad.gomez@elca.ch>
2025-02-04 11:54:22 +0100

    no message

--- sonar-scanner-maven/maven-basic/src/main/java/com/acme/basic/HelloWorld.java
+++ sonar-scanner-maven/maven-basic/src/main/java/com/acme/basic/HelloWorld.java
@@ -1,13 +1,19 @@
 package com.acme.basic;
 
+import java.util.logging.Logger;
+
 public class HelloWorld {
 
   void sayHello() {
-    System.out.println("Hello World!");
+    Logger log = Logger.getLogger("HelloWorld");
+
+    log.info("Hello World");
   }
 
   void notCovered() {
-    System.out.println("This method is not covered by unit tests");
+    Logger log = Logger.getLogger("This method is not covered by unit tests");
+
+    log.info("This is a test");
   }
 
 }
